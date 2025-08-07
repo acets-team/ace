0ae7bdff88bc6125fb9a5299ea2f4764c302bb73
Chris Carrington <carrington.christopher@gmail.com>
2025-08-07 11:58:34 -0700

    instanceof AceError causes import buildUrl in respond

--- src/fundamentals/respond.ts
+++ src/fundamentals/respond.ts
@@ -5,8 +5,10 @@
  */
 
 
-import { AceError } from './aceError'
+
+import type { AceError } from './aceError'
 import type { AceResponse, ApiResponse } from './types'
+
 
 
 /**
@@ -18,27 +20,37 @@ import type { AceResponse, ApiResponse } from './types'
  * @param props.headers - Optional, HTTP headers, automatically adds a content type of application json
  */
 export function respond<T_Data>({ data, error, go, status, headers }: RespondProps<T_Data>): AceResponse<T_Data> {
-  if (go) {
-    const h = new Headers(headers)
-    h.set('Location', go)
-    const s = [301, 302, 303, 307, 308].includes(status) ? status : 301
+  if (go) return giveRedirect(status, go, headers)
+  else return giveData(status, data ?? null, error ?? null, headers)
+}
 
-    return new Response(null, { headers: h, status: s })
-  }
 
-  const h = new Headers(headers)
-  h.set('content-type', 'application/json')
-  const init: ResponseInit = { status, headers: h }
+
+function giveRedirect(s: number, go: string, h?: HeadersInit) {
+  const headers = new Headers(h)
+  headers.set('Location', go)
+  const status = [301, 302, 303, 307, 308].includes(s) ? s : 301 // varing out these codes into a Set causes errors @ cloudflare workers b/c they need to know at build time is this a redirect
+
+  return new Response(null, { headers, status })
+}
+
+
+
+function giveData<T_Data>(status: number, data: T_Data | null, error: AceError | null, h?: HeadersInit) {
+  const headers = new Headers(h)
+  headers.set('content-type', 'application/json')
+  const init: ResponseInit = { status, headers }
   const responseJSON: ApiResponse<T_Data> = {}
 
   if (data !== null && data !== undefined) responseJSON.data = data
-  if (error instanceof AceError) responseJSON.error = error.get().error
+  if (error) responseJSON.error = error.get().error
 
   const res: AceResponse<T_Data> = new Response(JSON.stringify(responseJSON), init)
   res.__dataType = null as T_Data
 
   return res
 }
+
 
 
 export type RespondProps<T_Data> = {
