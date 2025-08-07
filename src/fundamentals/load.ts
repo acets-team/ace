008b546ffc91af96d6aa4fa1a117be94a363caef
Chris Carrington <carrington.christopher@gmail.com>
2025-08-06 18:02:41 -0700

    Update load.ts

--- src/fundamentals/load.ts
+++ src/fundamentals/load.ts
@@ -5,7 +5,8 @@
 
 
 import { AceError } from './aceError'
-import { query, createAsync, AccessorWithLatest } from '@solidjs/router'
+import { query, createAsync } from '@solidjs/router'
+import { createSignal, onMount, type Accessor } from 'solid-js'
 
 
 /**
@@ -24,24 +25,49 @@ import { query, createAsync, AccessorWithLatest } from '@solidjs/router'
     ```
  * @param fetchFn - Anonymous async function
  * @param cacheKey - Helps browser cache this data for back button, or multi calls on page, but a page refresh is always fresh data and can be used w/ our `reload()` or Solid's `revalidate()` https://docs.solidjs.com/solid-router/reference/data-apis/revalidate
+ * @param apiSetsCookies - 🚨 If the api sets cookies then this must be true. This will ensure the browser sends this request and then the browser will recieve the response w/ Set-Cookie headers.
  */
-export function load<T_Response>(fetchFn: () => Promise<T_Response>, cacheKey: string): AccessorWithLatest<T_Response | undefined> {
-  const loaded = query(fetchFn, cacheKey) // if a redirect status is returned here, the redirect happens & `createAsync()` does not run
+export function load<T_Response>( fetchFn: () => Promise<T_Response>, cacheKey: string, apiSetsCookies?: boolean ): Accessor<T_Response | undefined> {
+  if (apiSetsCookies) {
+    const [response, setResponse] = createSignal<T_Response>()
 
-  return createAsync(async () => {
-    try {
-      const response = await loaded()
+    onMount(async () => {
+      try {
+        const response = await fetchFn()
 
-      if (response instanceof Response) {
-        const clonedResponse = response.clone()
-        const data = await clonedResponse.json() as T_Response
+        if (response instanceof Response) {
+          const clonedResponse = response.clone()
+          const data = await clonedResponse.json() as T_Response
 
-        return data
+          setResponse(() => data)
+        }
+
+        setResponse(() => response)
+      } catch (error) {
+        const e =  await AceError.catch(error) as T_Response
+        setResponse(() => e)
       }
+    })
 
-      return response as T_Response
-    } catch (error) {
-      return await AceError.catch(error) as T_Response
-    }
-  }, {deferStream: true})
+    return response
+  } else {
+    const loaded = query(fetchFn, cacheKey) // if a redirect status is returned here, the redirect happens & `createAsync()` does not run
+
+    return createAsync(async () => {
+      try {
+        const response = await loaded()
+
+        if (response instanceof Response) {
+          const clonedResponse = response.clone()
+          const data = await clonedResponse.json() as T_Response
+
+          return data
+        }
+
+        return response as T_Response
+      } catch (error) {
+        return await AceError.catch(error) as T_Response
+      }
+    }, {deferStream: true})
+  }
 }
