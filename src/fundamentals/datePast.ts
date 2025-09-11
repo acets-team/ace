b2aaa02134805425ebe832cecdee157ac35d61f8
Chris Carrington <carrington.christopher@gmail.com>
2025-09-10 20:35:47 -0700

    agBooleanFilter + datePast

---
+++ src/fundamentals/datePast.ts
@@ -0,0 +1,17 @@
+/**
+ * 🧚‍♀️ How to access:
+ *     - import { datePast } from '@ace/datePast'
+ */
+
+
+import type { DateLike } from './types'
+import { dateLike2Date } from './dateLike2Date'
+
+
+/**
+ * ### True if date is not now and not in the future
+ * @param d - Parsed via `dateLike2Date()`
+ */
+export function datePast(d: DateLike) {
+  return dateLike2Date(d) < new Date()
+}
