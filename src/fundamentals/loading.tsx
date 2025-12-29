47e6468fcde692ddd5f2f6e2cb69baf26e73002d
Chris Carrington <carrington.christopher@gmail.com>
2025-12-28 19:50:35 -0800

    loading createStyleFactory + ace-spin

--- src/fundamentals/loading.tsx
+++ src/fundamentals/loading.tsx
@@ -1,11 +1,14 @@
 /**
  * 🧚‍♀️ How to access:
+ *     - Plugin: solid
  *     - import { Loading } from '@ace/loading'
+ *     - import type { LoadingProps } from '@ace/loading'
  */
 
 
-import type { JSX, Component } from 'solid-js'
-import { mergeObjects, mergeStrings } from './merge'
+import type { JSX } from 'solid-js'
+import { mergeStrings } from './merge'
+import { createStyleFactory } from './createStyleFactory'
 
 
 /**
@@ -15,82 +18,61 @@ import { mergeObjects, mergeStrings } from './merge'
   ```tsx
   <Loading color="black" />
 
-  <Loading type="two" color="black" twoColor="white" />
+  <Loading type="two" color="red" twoColor="blue" />
 
   <Loading label="Processing..." $span={{ id: "loader" }} />
   ```
-  @example
-  ```css
-  .ace-loading {
-    --ace-loading-color: var(--primary-color);
-  }
-
-  .brand {
-    color: white;
-    border: none;
-    background-color: var(--primary-color);
-
-    .ace-loading {
-      --ace-loading-color: white;
-    }
-  }
-  ```
- * 
- * ### 🎨 Custom CSS Variables:
- * - `--ace-loading-color`: Primary color, default: `gold`
- * - `--ace-loading-two-color`: Secondary color for `type="two"`, default: `white`
- * - `--ace-loading-size`: Spinner height & width, default: `2.1rem`
- * - `--ace-loading-thickness`: Spinner thickness, default: `0.3rem`
- * - `--ace-loading-speed`: Spin speed, default: `1s`
- * 
- * @param props.type - Optional, spinner type: `'one' (default) or 'two'`
- * @param props.size - Optional, spinner height & width, `default: 2.1rem`
- * @param props.thickness - Optional, spinner thickness, `default: 0.3rem`
- * @param props.speed - Optional, spinner speed, `default: 1s`
- * @param props.twoColor - Optional, if type is `two`, this will set the color for the 2nd spinner, `default: white`
- * @param props.label - Optional, text to announce to screen readers, `default: 'Loading...'`
- * @param props.$span - Optional, additional props to spread onto the outer `span`
+ * @param props.variant Optional, defaults to `one`, IF `one` THEN one spinner shows, IF `two` THEN two spinners show
+ * @param props.color Optional, color of first spinner, IF defined THEN sets value of `--ace-loading-color
+ * @param props.size Optional, spinner height/width, IF defined THEN sets value of `--ace-loading-size`
+ * @param props.thickness Optional, thickness of spinner, IF defined THEN sets value of `--ace-loading-thickness`
+ * @param props.speed Optional, spinning speed, IF defined THEN sets value of `--ace-loading-speed`
+ * @param props.twoColor Optional, IF `props.variant` is `two` THEN `props.twoColor` will set the color for the 2nd spinner w/ the css variable `--ace-loading-two-color`
+ * @param props.label Optional, text to announce to screen readers, `default: 'Loading...'`
+ * @param props.$span Optional, html props to add to wrapper span, class of `ace-loading` is added by default, IF setting `style` THEN must be of type object
  */
-export const Loading: Component<LoadingProps> = ({ type, size, thickness, color, speed, twoColor, label, $span }) => {
-  const requestedStyle = typeof $span?.style === 'object' ? $span.style : {}
+export function Loading (props: {
+  /** Optional, defaults to `one`, IF `one` THEN one spinner shows, IF `two` THEN two spinners show */
+  variant?: 'one' | 'two'
+  /** Optional, color of first spinner, IF defined THEN sets value of `--ace-loading-color` */
+  color?: string
+  /** Optional, spinner height/width, IF defined THEN sets value of `--ace-loading-size` */
+  size?: string
+  /** Optional, thickness of spinner, IF defined THEN sets value of `--ace-loading-thickness` */
+  thickness?: string
+  /** Optional, spinning speed, IF defined THEN sets value of `--ace-loading-speed` */
+  speed?: string
+  /** Optional, IF `props.variant` is `two` THEN `props.twoColor` will set the color for the 2nd spinner w/ the css variable `--ace-loading-two-color` */
+  twoColor?: string
+  /** Optional, text to announce to screen readers, `default: 'Loading...'` */
+  label?: string
+  /** Optional, html props to add to wrapper span, class of `ace-loading` is added by default, IF setting `style` THEN must be of type object */
+  $span?: JSX.HTMLAttributes<HTMLSpanElement>
+}) {
+  if (props.variant == undefined) props.variant = 'one'
 
-  const propsStyle: JSX.CSSProperties = {}
-
-  if (size) propsStyle['--ace-loading-size'] = size
-  if (thickness) propsStyle['--ace-loading-thickness'] = thickness
-  if (color) propsStyle['--ace-loading-color'] = color
-  if (speed) propsStyle['--ace-loading-speed'] = speed
-  if (twoColor) propsStyle['--ace-loading-two-color'] = twoColor
+  const { sm, createStyle } = createStyleFactory({ componentProps: props, requestStyle: props.$span?.style })
+  
+  const style = createStyle([
+    sm('size', '--ace-loading-size'),
+    sm('thickness', '--ace-loading-thickness'),
+    sm('color', '--ace-loading-color'),
+    sm('speed', '--ace-loading-speed'),
+    sm('twoColor', '--ace-loading-two-color'),
+  ])
 
   return <>
     <span
       role="status"
       aria-live="polite"
-      {...$span}
-      style={mergeObjects({ base: propsStyle, request: requestedStyle })}
-      class={mergeStrings('ace-loading', type === 'two' ? 'ace-loading--two' : '', $span?.class)}
+      {...props.$span}
+      style={style()}
+      class={mergeStrings('ace-loading', props.$span?.class, props.variant === 'two' ? 'ace-loading--two' : '')}
     >
-      <span class="label">{label || 'Loading...'}</span>
+      <span class="label">{props.label || 'Loading...'}</span>
     </span>
   </>
 }
 
 
-export type LoadingProps = {
-  /** Optional, spinner type: 'one' (default) or 'two' */
-  type?: 'one' | 'two'
-  /** Optional,  spinner height & width, default: `2.1rem` */
-  size?: string
-  /** Optional, spinner thickness, `default: 0.3rem` */
-  thickness?: string
-  /** Optional, spinner color, `default: gold` */
-  color?: string
-  /** Optional, spinner speed, `default: 1s` */
-  speed?: string
-  /** Optional, if type is `two`, this will set the color for the 2nd spinner, `default: white` */
-  twoColor?: string
-  /** Optional, text to announce to screen readers, `default: 'Loading...'` */
-  label?: string
-  /** Optional, additional props to spread onto the outer `span` */
-  $span?: JSX.HTMLAttributes<HTMLSpanElement>
-}
+export type LoadingProps = Parameters<typeof Loading>[0]
